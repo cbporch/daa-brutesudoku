27779261695e3d4979ca380d26bb51fb1f4f0726
Christopher Porch <porchc0@students.rowan.edu>
2015-09-09 10:44:06 -0400

    Added imports, added to main, added testInput.txt file
    Signed-off-by: Christopher Porch <porchc0@students.rowan.edu>

--- src/Driver.java
+++ src/Driver.java
@@ -5,14 +5,24 @@
  *  Authors: Christopher Porch <porchc0@students.rowan.edu>
  *  version: 2015.9.9
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Driver {
-
+	public static BufferedReader br;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new FileReader("\\testInput.txt"));
 		
+		int w = 0, h = 0;
+		try{
+			w = br.read();
+			h = br.read();
+		}catch(Exception e){
+			System.out.println("Error reading from file");
+		}
+		int sdk[][] = new int[w*h][w*h];
 	}
 
 }
