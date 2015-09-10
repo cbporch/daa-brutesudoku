c07e587dd7e5084358df99a34ec781f4359ab7d2
Dan Boehmke <Danboe2009@gmail.com>
2015-09-10 16:18:20 -0400

    Testing

--- src/sudokuPackage/Driver.java
+++ src/sudokuPackage/Driver.java
@@ -5,7 +5,7 @@ package sudokuPackage;
  *  Class: Design and Analysis of Algorithms
  *  Professor Lobo
  *  Authors: 	 Christopher Porch <porchc0@students.rowan.edu>
- *  			Dan Boehmke <>
+ *  			 Dan Boehmke <boehmked2@students.rowan.edu>
  *  version: 2015.9.9
  */
 
@@ -47,10 +47,32 @@ public class Driver {
 			} // end while
 			System.out.println("");
 		} // end for
-
+		
+		//checkRow Tester
+		int[] trueArray = new int[] {1,4,3,2,5,6};
+		int[] falseArray = new int[] {1,2,3,1,2,3};
+		System.out.println("Should be true: " + checkRowColumn(trueArray));
+		System.out.println("Should be false: " + checkRowColumn(falseArray));
+		
 	}// end main
 	
 	// Methods for checking rows/columns/blocks will go here?
+	// I think its best to make each thing a separate method - Dan
 	// Or, they will just be a part of main
+	
+	public static boolean checkRowColumn(int[] nums)
+	{
+		for(int i = 0; i < nums.length; i++) 
+		{
+			for(int j = 0; j < nums.length; j++)
+			{
+				if(i!=j && nums[i] == nums[j])
+				{
+					return false;
+				}
+			}// end j for
+		}// end i for
+		return true;
+	}// end CheckRowColumn
 
 }// end Driver
