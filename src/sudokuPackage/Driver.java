16a98756cf1021b1f1173e9e4fae017008d7f94e
Christopher Porch <cbporch@gmail.com>
2015-09-10 23:34:48 -0400

    added code to utilize checkRowColumn method
    Signed-off-by: Christopher Porch <cbporch@gmail.com>

--- src/sudokuPackage/Driver.java
+++ src/sudokuPackage/Driver.java
@@ -4,8 +4,9 @@ package sudokuPackage;
  *  Project: Brute Force Sudoku Solver
  *  Class: Design and Analysis of Algorithms
  *  Professor Lobo
- *  Authors: 	 Christopher Porch <porchc0@students.rowan.edu>
- *  			 Dan Boehmke <boehmked2@students.rowan.edu>
+ *  Authors: 	Christopher Porch <porchc0@students.rowan.edu>
+ *  			Dan Boehmke <boehmked2@students.rowan.edu>
+ *  			Bryan <>
  *  version: 2015.9.9
  */
 
@@ -29,50 +30,84 @@ public class Driver {
 		} catch (Exception e) {
 			System.out.println("Error reading from file");
 		} // end try catch
-		
+
 		int sdk[][] = new int[w * h][w * h];
 		int y;
-		
+
 		// Use regex to pull numbers out of the string read in by
 		// BufferedReader br.readLine() and populate 2D array sdk[][]
 		Pattern p = Pattern.compile("\\d+");
 		for (int x = 0; x < (w * h); x++) {
 			y = 0;
 			Matcher m = p.matcher(br.readLine());
-			
+
 			while (m.find() && y < (w * h)) {
 				sdk[x][y] = Integer.parseInt(m.group());
 				System.out.print(sdk[x][y] + " ");
 				y++;
 			} // end while
 			System.out.println("");
+			
 		} // end for
-		
-		//checkRow Tester
-		int[] trueArray = new int[] {1,4,3,2,5,6};
-		int[] falseArray = new int[] {1,2,3,1,2,3};
+
+		// checkRow Tester
+		int[] trueArray = new int[] { 1, 4, 3, 2, 5, 6 };
+		int[] falseArray = new int[] { 1, 2, 3, 1, 2, 3 };
 		System.out.println("Should be true: " + checkRowColumn(trueArray));
 		System.out.println("Should be false: " + checkRowColumn(falseArray));
+
+		/*
+		 * Somewhere in here should go code to check for zeros, and start to fill them in with
+		 * potential numbers. I think maybe we should duplicate our 2D array each time and test on that 
+		 * one, so as not to mess up the original too badly while searching? That or somehow keep
+		 * track of the indexes the zeros are in.
+		 * 															-Chris
+		 */
+		
+		// have not tested this block yet, might need to be inside a bigger while loop 
+		// but it should test one row and one column at a time, so it only has to
+		// run (w * h) times instead of 2(w * h) times              -Chris
+		int tempRow[] = new int[w*h];
+		int tempCol[] = new int[w*h];
+		for(int i = 0;i < (w*h);i++) {
+			for(int j = 0;j < (w*h);j++) {
+				tempRow[j] = sdk[i][j]; // pulls numbers from each row
+				tempCol[j] = sdk[j][i]; // pulls number from each column
+			}// end for
+			
+			if(checkRowColumn(tempRow)) {
+				// catch potential row
+			} else {
+				// fail, exit loop, try new numbers?
+				i += (w * h);
+			}// end if else
+		
+			if(checkRowColumn(tempCol)) {
+				// catch potential column
+			} else {
+				// fail, exit loop, try new numbers?
+				i += (w * h);
+			}// end if else
+		}// end for
+		
 		
 	}// end main
-	
-	// Methods for checking rows/columns/blocks will go here?
-	// I think its best to make each thing a separate method - Dan
-	// Or, they will just be a part of main
-	
-	public static boolean checkRowColumn(int[] nums)
-	{
-		for(int i = 0; i < nums.length; i++) 
-		{
-			for(int j = 0; j < nums.length; j++)
-			{
-				if(i!=j && nums[i] == nums[j])
-				{
+
+	public static boolean checkRowColumn(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (i != j && nums[i] == nums[j]) {
 					return false;
 				}
-			}// end j for
-		}// end i for
+			} // end j for
+		} // end i for
 		return true;
 	}// end CheckRowColumn
-
+	
+	public static boolean checkBlocks(int[][] arr) {
+		return false;
+		// placeholder, needs to be written, idea is to break the 2D array into separate
+		// 1D array blocks, and use Dan's checkRowColumn to verify each has no duplicates 
+		// block by block
+	}// end checkBlocks
 }// end Driver
