b74e5c4dbb4c57a8ff62103bd6cf40f09bbcdbd2
Christopher Porch <cbporch@gmail.com>
2015-09-11 09:48:34 -0400

    Formatting
    Signed-off-by: Christopher Porch <cbporch@gmail.com>

--- src/sudokuPackage/Driver.java
+++ src/sudokuPackage/Driver.java
@@ -4,9 +4,9 @@ package sudokuPackage;
  *  Project: Brute Force Sudoku Solver
  *  Class: Design and Analysis of Algorithms
  *  Professor Lobo
- *  Authors: 	Christopher Porch <porchc0@students.rowan.edu>
- *  		Dan Boehmke <boehmked2@students.rowan.edu>
- *  		Brian Grillo <>
+ *  Authors:	Christopher Porch <porchc0@students.rowan.edu>
+ *  			Dan Boehmke <boehmked2@students.rowan.edu>
+ *  			Brian Grillo <>
  *  version: 2015.9.11
  */
 
@@ -47,7 +47,7 @@ public class Driver {
 				y++;
 			} // end while
 			System.out.println("");
-			
+
 		} // end for
 
 		// checkRow Tester
@@ -57,40 +57,41 @@ public class Driver {
 		System.out.println("Should be false: " + checkRowColumn(falseArray));
 
 		/*
-		 * Somewhere in here should go code to check for zeros, and start to fill them in with
-		 * potential numbers. I think maybe we should duplicate our 2D array each time and test on that 
-		 * one, so as not to mess up the original too badly while searching? That or somehow keep
-		 * track of the indexes the zeros are in.
-		 * 															-Chris
+		 * Somewhere in here should go code to check for zeros, and start to
+		 * fill them in with potential numbers. I think maybe we should
+		 * duplicate our 2D array each time and test on that one, so as not to
+		 * mess up the original too badly while searching? That or somehow keep
+		 * track of the indexes the zeros are in. -Chris
 		 */
-		
-		// have not tested this block yet, might need to be inside a bigger while loop 
-		// but it should test one row and one column at a time, so it only has to
-		// run (w * h) times instead of 2(w * h) times              -Chris
-		int tempRow[] = new int[w*h];
-		int tempCol[] = new int[w*h];
-		for(int i = 0;i < (w*h);i++) {
-			for(int j = 0;j < (w*h);j++) {
+
+		// have not tested this block yet, might need to be inside a bigger
+		// while loop
+		// but it should test one row and one column at a time, so it only has
+		// to
+		// run (w * h) times instead of 2(w * h) times -Chris
+		int tempRow[] = new int[w * h];
+		int tempCol[] = new int[w * h];
+		for (int i = 0; i < (w * h); i++) {
+			for (int j = 0; j < (w * h); j++) {
 				tempRow[j] = sdk[i][j]; // pulls numbers from each row
 				tempCol[j] = sdk[j][i]; // pulls number from each column
-			}// end for
-			
-			if(checkRowColumn(tempRow)) {
+			} // end for
+
+			if (checkRowColumn(tempRow)) {
 				// catch potential row
 			} else {
 				// fail, exit loop, try new numbers?
 				i += (w * h);
-			}// end if else
-		
-			if(checkRowColumn(tempCol)) {
+			} // end if else
+
+			if (checkRowColumn(tempCol)) {
 				// catch potential column
 			} else {
 				// fail, exit loop, try new numbers?
 				i += (w * h);
-			}// end if else
-		}// end for
-		
-		
+			} // end if else
+		} // end for
+
 	}// end main
 
 	public static boolean checkRowColumn(int[] nums) {
@@ -103,11 +104,13 @@ public class Driver {
 		} // end i for
 		return true;
 	}// end CheckRowColumn
-	
+
 	public static boolean checkBlocks(int[][] arr) {
 		return false;
-		// placeholder, needs to be written, idea is to break the 2D array into separate
-		// 1D array blocks, and use Dan's checkRowColumn to verify each has no duplicates 
+		// placeholder, needs to be written, idea is to break the 2D array into
+		// separate
+		// 1D array blocks, and use Dan's checkRowColumn to verify each has no
+		// duplicates
 		// block by block
 	}// end checkBlocks
 }// end Driver
